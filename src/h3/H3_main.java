10d1336b25eb10428881455339c6ff4c31465026
Aruna Anhuf <a.anhuf@mail.com>
2025-11-13 16:44:17 +0100

    H3

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -2,7 +2,13 @@ package h3;
 
 public class H3_main {
 	public static void main(String[] args) {
-		
-		
+		float celsiusFloat = 14.2f, fahrenheitFloat = 3.85f;
+		double celsiusDouble = 14.2, fahrenheitDouble = 8.1;
+
+		fahrenheitFloat = celsiusFloat * 9 / 5 + 32;
+		fahrenheitDouble = celsiusDouble * 9 / 5 + 32;
+
+		System.out.println("fahrenheitDouble = " + fahrenheitDouble);
+		System.out.println("fahrenheitFloat = " + fahrenheitFloat);
 	}
 }
